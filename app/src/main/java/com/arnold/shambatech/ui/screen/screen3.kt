2a3176b003a88c17ce5eb135e7577b01ea3eaa4b
abe1dac <arnoldodwory06@gmail.com>
2025-01-26 22:22:00 +0300

    good

---
+++ app/src/main/java/com/arnold/shambatech/ui/screen/screen3.kt
@@ -0,0 +1,10 @@
+package com.arnold.shambatech.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Screen1(modifier: Modifier = Modifier) {
+    Text(text = "welcome Back ")
+}
