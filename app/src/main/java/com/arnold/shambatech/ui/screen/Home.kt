259236c46feb5e6368d439b290d1b6f7524a2d8a
abe1dac <arnoldodwory06@gmail.com>
2025-01-29 23:57:55 +0300

    good

---
+++ app/src/main/java/com/arnold/shambatech/ui/screen/Home.kt
@@ -0,0 +1,9 @@
+package com.arnold.shambatech.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+    
+}
