07576a90c29de070643fe4a408fb0f068bb84cce
abe1dac <arnoldodwory06@gmail.com>
2025-01-30 00:04:35 +0300

    good

--- app/src/main/java/com/arnold/shambatech/ui/screen/Home.kt
+++ app/src/main/java/com/arnold/shambatech/ui/screen/Home.kt
@@ -1,9 +1,10 @@
 package com.arnold.shambatech.ui.screen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Home(modifier: Modifier = Modifier) {
-    
+    Text(text = "Welcome to ShambaTech")
 }
--- app/src/main/java/com/arnold/shambatech/ui/screen/Screen2.kt
+++ app/src/main/java/com/arnold/shambatech/ui/screen/Screen2.kt
@@ -1,9 +1,13 @@
 package com.arnold.shambatech.ui.screen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun Screen2(modifier: Modifier = Modifier) {
-    
+fun Screen2(modifier: Modifier = Modifier,
+            horizontal: Alignment.Horizontal,
+            vertical: Alignment. Horizontal) {
+    Text(text = "Welcome to ShambaTech")
 }
